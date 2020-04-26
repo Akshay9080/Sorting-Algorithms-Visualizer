d9681e119ef02fefed324a6e00802342f6d3c7a2
Akshay9080 <akshaybmali01@gmail.com>
2020-04-26 19:49:44 +0530

    Implementing Select, Insert and Bubble sort

--- sorting-algorithm-visualizer/src/App.js
+++ sorting-algorithm-visualizer/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Display from './Display';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    arr : [{id: 0, val: 10,col: "blue"},
+    {id: 1, val: 29,col: "blue"},
+    {id: 2, val: 37,col: "blue"},
+    {id: 3, val: 25,col: "blue"},
+    {id: 4, val: 15,col: "blue"},
+    {id: 5, val: 12,col: "blue"}
+    ]
+  }
+
+  generateArray = (num) => {
+    var A = [];
+    for(var i=0;i<num;i++){
+      var ele = {
+        id: i,
+        val: Math.round(Math.random() * 55) + 5,
+        col: "blue"
+      }
+      A.push(ele);
+    }
+    this.setState({arr: A});
+  }
+
+  updateArray = (A) => {
+    var a = A;
+    this.setState({arr: a});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Display arr = {this.state.arr} updateArray = {this.updateArray} generateArray = {this.generateArray} />
+      </div>
+    );
+  }
 }
 
 export default App;
